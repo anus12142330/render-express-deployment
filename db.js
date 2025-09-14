7d7d4229fe06036db6566e38d17b725468cf0fa0
anus12142330 <49470936+anus12142330@users.noreply.github.com>
2025-09-14 11:24:42 +0400

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@
 import mysql from "mysql2";
 
 const db = mysql.createPool({
-  host: "localhost", //process.env.DB_HOST || "localhost",
-  user: "admin_portaldb", //process.env.DB_USER || "root",
-  password: "JvfD6C2CpFcmGvXQHcsZ", //process.env.DB_PASSWORD || "",
-  database: "portal_db",  //process.env.DB_NAME || "portal_db",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "portal_db",
   port: Number(process.env.DB_PORT || 3306),
   waitForConnections: true,
   connectionLimit: 10,
