fc11708c771084aedff0c393ea552ef9c506f68c
anus12142330 <49470936+anus12142330@users.noreply.github.com>
2026-02-22 14:13:01 +0400

    Delete migrations directory

--- migrations/20260120_remove_temperature_logger_from_qc_inspections.sql
+++
@@ -1,4 +0,0 @@
--- Remove temperature logger fields from qc_inspections table
-ALTER TABLE qc_inspections
-  DROP COLUMN IF EXISTS temperature_logger_received,
-  DROP COLUMN IF EXISTS temperature_logger_file_path;
--- migrations/add_shipment_id_to_ap_bills.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE ap_bills
-ADD COLUMN shipment_id INT NULL AFTER purchase_order_id,
-ADD INDEX idx_ap_bills_shipment_id (shipment_id);
-
--- Optional FK (enable if shipment table exists and you want strict integrity)
--- ALTER TABLE ap_bills
--- ADD CONSTRAINT fk_ap_bills_shipment_id
--- FOREIGN KEY (shipment_id) REFERENCES shipment(id)
--- ON DELETE SET NULL;
