2a1791fd63eb4e93e82c301ad882293abbd64f26
anus12142330 <49470936+anus12142330@users.noreply.github.com>
2026-04-29 17:52:32 +0400

    Delete tmp directory

--- tmp/debug_dispatch.js
+++
@@ -1,9 +0,0 @@
-import db from '../db.js';
-import fs from 'fs';
-async function test() {
-    const [dispatches] = await db.promise().query('SELECT * FROM sales_order_dispatches ORDER BY id DESC LIMIT 5');
-    const [attachments] = await db.promise().query('SELECT * FROM sales_order_attachments ORDER BY id DESC LIMIT 10');
-    fs.writeFileSync('debug_out.txt', JSON.stringify({ dispatches, attachments }, null, 2));
-    process.exit();
-}
-test();
